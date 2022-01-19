50ec3419bed69728adc7dd1c548bd4da587c147c
johnespinheiro <johnes.armt@gmail.com>
2022-01-19 17:17:14 -0300

    adding get method

---
+++ APICatalogo/APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,42 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+           return _context.Produtos.ToList();
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Produto> get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return produto;
+        }
+        [HttpPost]
+        public ActionResult Post([FromBody]Produto produto)
+        {
+
+        }
+    }
+}
