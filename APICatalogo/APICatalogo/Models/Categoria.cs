38b3bf7717d5618190ecaf70757ea80a4d013f0a
johnespinheiro <johnes.armt@gmail.com>
2022-01-19 13:01:56 -0300

    migrations

--- APICatalogo/APICatalogo/Models/Categoria.cs
+++ APICatalogo/APICatalogo/Models/Categoria.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace APICatalogo.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
         //quando tem definição de coleção (public icollection), tem que definir a inicialização dessa coleção
@@ -16,8 +19,13 @@ namespace APICatalogo.Models
             //minha propriedade Produtos é um new Collection de produto
             Produtos = new Collection<Produto>();
         }
+        [Key]
         public int CategoriaId { get; set; }
+        [Required]  //recurso de data anotation para definir propriedades ( requerido e 80 caracteres )
+        [MaxLength(80)]
         public string Nome { get; set; }
+        [Required]
+        [MaxLength(300)]
         public string ImagemUrl { get; set; }
 
         // uma categoria possui muitos produtos, entao defino uma coleção de produtos em categorias
